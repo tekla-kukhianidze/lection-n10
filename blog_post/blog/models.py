1ccdde45a2a57fb02dfc48164982f01843371360
tekla-hub <teklakukhianidze93@gmail.com>
2025-11-10 00:51:54 +0400

    commit changes

--- blog_post/blog/models.py
+++ blog_post/blog/models.py
@@ -4,10 +4,17 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(verbose_name="სათაური", max_length=255)
     text = models.TextField(verbose_name="ტექსტი")
+    active=models.BooleanField(default=True, verbose_name='აქტიურია')
+    create_date=models.DateTimeField(verbose_name='შექმნის თარიღი', auto_now_add=True, null=True)
+    update_date=models.DateTimeField(verbose_name='განახლების თარიღი', auto_now=True,null=True)
+    picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    document = models.FileField(upload_to='documents/', null=True, blank=True)
+
 
     class Meta:
         verbose_name = "Blog Post"
         verbose_name_plural = "Blog Posts"
+        ordering=['title']
 
     def __str__(self):
         return self.title
