679cdce104a0bc8db9cc93b4a533394093a39cbc
tekla-hub <teklakukhianidze93@gmail.com>
2025-11-10 21:58:34 +0400

    commit changes

--- blog_post/blog/admin.py
+++ blog_post/blog/admin.py
@@ -1,4 +1,8 @@
+
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, BlogPostImage, Author, BannerImage
 
 admin.site.register(BlogPost)
+admin.site.register(BlogPostImage)
+admin.site.register(Author)
+admin.site.register(BannerImage)
--- blog_post/blog/models.py
+++ blog_post/blog/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 
+class Author(models.Model):
+    first_name = models.CharField(verbose_name="სახელი", max_length=100)
+    last_name = models.CharField(verbose_name="გვარი", max_length=100)
+    email = models.EmailField(verbose_name="მეილი")
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name
+
 
 class BlogPost(models.Model):
+    authors = models.ManyToManyField(to='blog.Author', verbose_name="ავტორები", related_name="blog_posts")
     title = models.CharField(verbose_name="სათაური", max_length=255)
     text = models.TextField(verbose_name="ტექსტი")
     active=models.BooleanField(default=True, verbose_name='აქტიურია')
     create_date=models.DateTimeField(verbose_name='შექმნის თარიღი', auto_now_add=True, null=True)
     update_date=models.DateTimeField(verbose_name='განახლების თარიღი', auto_now=True,null=True)
-    picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
     document = models.FileField(upload_to='documents/', null=True, blank=True)
+
 
 
     class Meta:
@@ -20,6 +33,30 @@ class BlogPost(models.Model):
         return self.title
 
 
-from django.db import models
+class BannerImage(models.Model):
+    blog_post = models.OneToOneField(
+        to='blog.BlogPost',
+        related_name='banner_image',
+        on_delete=models.CASCADE,
+    )
+    image = models.ImageField(upload_to='banner_image/', null=True, blank=True)
 
-# Create your models here.
+    class Meta:
+        verbose_name = "Banner Image"
+        verbose_name_plural = "Banner Images"
+
+    def __str__(self):
+        return f'{self.blog_post.title} - {self.id} image'
+
+
+class BlogPostImage(models.Model):
+    blog_post = models.ForeignKey(to='blog.BlogPost', related_name='images', on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='blog_image/')
+
+
+    class Meta:
+        verbose_name = "Blog Post Image"
+        verbose_name_plural = "Blog Post Images"
+
+    def __str__(self):
+        return f'{self.blog_post.title} - {self.id} image'
